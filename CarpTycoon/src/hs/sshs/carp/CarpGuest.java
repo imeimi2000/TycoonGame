6ca796ae0a8e89ff7ff39fd8100e54ad574ad920
babyb <babyb@DESKTOP-6JIR50T>
2017-12-07 12:02:34 +0900

    Add 호빵맨

--- CarpTycoon/src/hs/sshs/carp/CarpGuest.java
+++ CarpTycoon/src/hs/sshs/carp/CarpGuest.java
@@ -3,13 +3,16 @@ package hs.sshs.carp;
 import java.awt.Graphics;
 
 public class CarpGuest extends GameObject {
+	private static final Point size = new Point(211, 200);
+	
 	CarpGuest(int x, int y) {
 		super(x, y);
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		
+		g.drawImage(ImageManager.findImage("guest_1"), location.getX(), location.getY()
+				, size.getX(), size.getY(), GameManager.getScreen());
 	}
 	
 	@Override
--- CarpTycoon/src/hs/sshs/carp/GameManager.java
+++ CarpTycoon/src/hs/sshs/carp/GameManager.java
@@ -1,6 +1,6 @@
 package hs.sshs.carp;
 
-import java.util.Queue;
+import java.util.ArrayList;
 
 public class GameManager {
 	private static GameManager mainManager;
@@ -9,7 +9,9 @@ public class GameManager {
 	private GameScreen scr;
 	private int frameRate;
 	private long startTime;
-	private Queue<CarpGuest> guest;
+	private long frameCount;
+	private ArrayList<CarpGuest> guest;
+	private final int maxGuestCount = 3;
 	private Carp[] cast = new Carp[9];
 	
 	public static long currentTime() {
@@ -19,10 +21,12 @@ public class GameManager {
 	public GameManager(String title, int width, int height, int frameRate) {
 		scr = new GameScreen(title, width, height);
 		this.frameRate = frameRate;
+		frameCount = 0;
 		startTime = System.currentTimeMillis();
 		mainManager = this;
+		guest = new ArrayList<CarpGuest>();
 		for (int i = 0; i < cast.length; ++i) {
-			cast[i] = new Carp(250 + 200 * (i / 3), 720 - (107 * (i % 3 + 1)));
+			cast[i] = new Carp(400 + 200 * (i / 3), 720 - (107 * (i % 3 + 1)));
 			scr.addObject(cast[i], 1);
 		}
 	}
@@ -32,9 +36,18 @@ public class GameManager {
 	public void update() {
 		updateTime = System.currentTimeMillis();
 		
+		if (guest.size() < maxGuestCount && frameCount % (frameRate * 3) == 0) {
+			CarpGuest newGuest = new CarpGuest(100, 0);
+			guest.add(newGuest);
+			scr.addObject(newGuest, 0);
+		}
 		
+		for (int i = 0; i < guest.size(); ++i) {
+			guest.get(i).setY(720 - 200 * (i + 1));
+		}
 		
 		scr.repaint();
+		frameCount++;
 	}
 	
 	public static GameManager getManager() {
